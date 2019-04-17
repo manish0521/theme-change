03ea5aad47178c952ed469d26217dd8374d69efa
manish singh <manish.singh@codeimmersives.com>
2019-04-17 16:29:04 -0400

    emojis added and worked

--- main.js
+++ main.js
@@ -8,21 +8,65 @@ function init() {
 function toggleTheme(event) {
     event.preventDefault();
 
+    const app = document.querySelector('#app');
     const half = document.querySelector('.half');
     const bottomHalf = document.querySelector('.bottom-half');
-    const button = event.target;
 
-    half.classList.toggle('bottom-half');
-    bottomHalf.classList.toggle('half');
    
 
-    // button.innerText = button.innerText === 'SWITCH COLOR'
-    //     ? `SWITCH TO START`
-    //     : `SWITCH COLOR`;
+    app.classList.toggle('app');
+    half.classList.toggle('bottom-half');
+    bottomHalf.classList.toggle('half');
+    
 
+    const button = document.querySelector('.toggle-button');
+    
+   
+
+    button.innerText = button.innerText === 'SWITCH BACKGROUND'
+        ? `SWITCH TO START`
+        : `SWITCH BACKGROUND`;
+
+    app.classList.replace('app', 'alt-app');
     half.classList.replace('half', 'bottom-half');
     bottomHalf.classList.replace('bottom-half', 'half');
-
-
     
+    
+
+    // Query table 1 and table 2.
+    const res1 = document.querySelector('#res');
+    const prj1 = document.querySelector('#prj');
+
+    const res2 = document.querySelector('#res2');
+    const prj2 = document.querySelector('#prj2');
+
+    const res3 = document.querySelector('#res3');
+    const prj3 = document.querySelector('#prj3');
+    
+    // // Add table 1 as a child to projects
+    res1.innerText = res1.innerText === '🧥'
+    ? `🙏🏼`
+    : `🧥`;
+    
+    res2.innerText = res2.innerText === '👖'
+    ? `👍🏼`
+    : `👖`;
+
+    res3.innerText = res3.innerText === '👞'
+    ? `👎🏼`
+    : `👞`;
+    
+    // Add table 2 as a child to resume.
+
+    prj1.innerText = prj1.innerText === '🙏🏼'
+    ? `🧥`
+    : `🙏🏼`;
+
+    prj2.innerText = prj2.innerText === '👍🏼'
+    ? `👖`
+    : `👍🏼`;
+
+    prj3.innerText = prj3.innerText === '👎🏼'
+    ? `👞`
+    : `👎🏼`;
 }
