ba79b43d9dc098642e4c3f885ea7cd002f2bdf8a
Manish Singh <46725819+manish0521@users.noreply.github.com>
2019-04-17 12:19:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+window.onload = init;
+
+function init() {
+    document.querySelector('.toggle-button')
+        .addEventListener('click', toggleTheme);
+}
+
+function toggleTheme(event) {
+    event.preventDefault();
+
+    const half = document.querySelector('.half');
+    const bottomHalf = document.querySelector('.bottom-half');
+    const button = event.target;
+
+    half.classList.toggle('bottom-half');
+    bottomHalf.classList.toggle('half');
+    // button.classList.toggle('toggle-button');
+
+    // button.innerText = button.innerText === 'SWITCH COLOR'
+    //     ? `SWITCH TO START`
+    //     : `SWITCH COLOR`;
+
+    half.classList.replace('half', 'bottom-half');
+    bottomHalf.classList.replace('bottom-half', 'half');
+
+
+    
+}
